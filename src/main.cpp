bb175b36263d30f7ead788361dd3dfeea5c10bc6
Bro Dev <me.ali.afif@gmail.com>
2026-06-25 14:11:20 +0100

    feat: linked GLAD and added window

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,38 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main(void)
+#include <iostream>
+
+int main()
 {
-    GLFWwindow* window;
-
     if (!glfwInit())
-        return -1;
-
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
     {
+        std::cout << "ERROR: Failed to initialize GLFW!" << std::endl;
+        return -1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Planetarium", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "ERROR: Failed to open window!" << std::endl;
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "ERROR: Failed to initialize GLAD!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glViewport(0, 0, 800, 600);
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
     while (!glfwWindowShouldClose(window))
     {
