8379731f07431b6460eae44d12e6caeee3138e7a
Bro Dev <me.ali.afif@gmail.com>
2026-06-25 15:51:34 +0100

    feat: added triangle (polygon rendering)

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
 int main()
 {
     if (!glfwInit())
@@ -32,11 +37,126 @@ int main()
         return -1;
     }
     glViewport(0, 0, 800, 600);
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    const char* vs_source = "#version 460 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+
+    const char* fs_source = "#version 460 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\0";
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vs_source, NULL);
+    glCompileShader(vertexShader);
+    
+    int  vs_success;
+    char vs_infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vs_success);
+    if(!vs_success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, vs_infoLog);
+        std::cout << "ERROR: Failed to compile vertex shader!\n" << vs_infoLog << std::endl;
+    }
+    else
+    {
+        std::cout << "Successfully compiled VEREX SHADER!" << std::endl;
+    }
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fs_source, NULL);
+    glCompileShader(fragmentShader);
+    
+    int  fs_success;
+    char fs_infoLog[512];
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fs_success);
+    if(!fs_success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, fs_infoLog);
+        std::cout << "ERROR: Failed to compile fragment shader!\n" << fs_infoLog << std::endl;
+    }
+    else
+    {
+        std::cout << "Successfully compiled FRAGMENT SHADER!" << std::endl;
+    }
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, fragmentShader);
+    glAttachShader(shaderProgram, vertexShader);
+    glLinkProgram(shaderProgram);
+
+    int  prg_success;
+    char prg_infoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &prg_success);
+    if(!prg_success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, prg_infoLog);
+        std::cerr << "ERROR: Failed to link shader program!\n" << prg_infoLog << std::endl;
+    }
+    else
+    {
+        std::cout << "Successfully linked SHADER PROGRAM!" << std::endl;
+    }
+
+    glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShader);
+
+    float vertices[] = {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    };
+    
+    unsigned int VBO;
+    unsigned int VAO;
+    unsigned int EBO;
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);  
+
+
+
+
+
+
 
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
